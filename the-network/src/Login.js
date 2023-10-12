5aea4b47033ec8511207c36ad41d3d01c16bec26
JamesKm08 <66277467+JamesKm08@users.noreply.github.com>
2023-10-12 09:37:30 +0300

    feat:create Login page

--- the-network/src/Login.js
+++ the-network/src/Login.js
@@ -1,22 +1,36 @@
-
+import "./Login.scss"
 
 export default function Login(){
     return(
         <>
-        <div id = "login-container">
-            <form>
-                <input
-                    type='email'
-                    placeholder='Email'
-                    
-                />
-                <input
-                    type='password'
-                    placeholder='Password'
-                
-                />
+        <div id = "login">
+            <div className="card">
+                <div className="left">
+                    <h1>Hello Friend!</h1>
+                    <p>ks;ib;beiveriverbvieb
+                        vlevjcvewglegfweiew,dubiuh
+                        ewhfoewhfowejfweflkwnfowhfewfwuf,cbwibw
+                        ebweibwecbwe;icwec.
+                    </p>
+                    <span>Don't have an account?</span>
+                    <button>Register</button>
+                </div>
+
+            <div className="right">
+                <h1>Login</h1>
+                <form>
+                    <input
+                        type='email'
+                        placeholder='Email'   
+                    />
+                    <input
+                        type='password'
+                        placeholder='Password'
+                    />
                 <button type='submit'>Login</button>
             </form>
+        </div>
+        </div>     
         </div>
         </>
     )
