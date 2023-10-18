ca2309e03fa5f331b48673478bcca24db34c63b5
JamesKm08 <66277467+JamesKm08@users.noreply.github.com>
2023-10-18 06:43:35 +0300

    feat: adding navbar icons

--- the-network/src/NavBar.js
+++ the-network/src/NavBar.js
@@ -1,23 +1,40 @@
 import { Link } from 'react-router-dom';
-
+import HomeIcon from '@mui/icons-material/Home';
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 
 export default function NavBar(){
     return(
   <>
-  <ul>
-  <li>Home</li>
+  <div className='navbar'>
+    <div className='left'>
+      <Link to='/' style={{textDecoration:'none'}}>
+      <span>The Network</span>
+      </Link>
+      <HomeIcon/>
+      <DarkModeOutlinedIcon/>
+      <GridViewOutlinedIcon/>
+      <div className='search'>
+      <SearchOutlinedIcon/>
+      <input type='text' placeholder='Search...'/>
+      </div>
+    </div>
 
-  <Link to = "/Login">
-    <h4>Login</h4>
-  </Link>
+    <div className='right'>
+      <PersonOutlinedIcon/>
+      <EmailOutlinedIcon/>
+      <NotificationsOutlinedIcon/>
+      <div className='user'>
+        <span> James Kamau</span>
+      </div>
+    </div>
 
-  <Link to = "/News">
-    <h4>News</h4>
-  </Link>
-    
-  <li></li>
-  <li>Profiles</li>
-  </ul>
+  </div>
   </>
     )
 }
