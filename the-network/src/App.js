f20cf6955ce821fe99e022def090f398e18c5bc0
JamesKm08 <66277467+JamesKm08@users.noreply.github.com>
2023-10-18 06:35:37 +0300

    feat: routing components

--- the-network/src/App.js
+++ the-network/src/App.js
@@ -1,19 +1,39 @@
 import './App.css';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 import NavBar from './NavBar';
 import Login from './Login';
-import News from './News';
+import Profile from './Profile';
+import Register from './Register';
+import LeftBar from './LeftBar';
+import RightBar from './RightBar';
+import Home from './Home';
 
 function App() {
+
+  const Layout = () =>{
+    return(<>
+      <div>
+        <NavBar/>
+        <div style ={{display: 'flex'}}>
+          <LeftBar/>
+          <Outlet/>
+          <RightBar/>
+        </div>
+      </div>
+      </>)
+  }
   return (<>
   <div className="App">
-    <NavBar/>
-    <Routes>
+    
 
-    <Route path="/Login" element={<Login/>}/>
-    <Route path="/News" element={<News/>}/>
-
-    </Routes>
+  <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" index element={<Home />} />
+        <Route path="/profile/:id" element={<Profile />} />
+      </Route>
+      <Route path="/Login" element={<Login />} />
+      <Route path="/Register" element={<Register />} />
+  </Routes>
     </div>
     </>
   );
